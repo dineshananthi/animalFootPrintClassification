fae4a4954893e1c74ff9f46cdb9bed1702a223f3
dineshkumar.anandan@zucisystems.com <dineshkumar.anandan@zucisystems.com>
2024-03-24 11:09:42 +0530

    commit-1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import FastAPI, logger
 from pydantic import BaseModel
 
+from core.vistra_afp_classification import vistra_afp_classification
+
 APP_TITLE = "ANIMAL FOOT PRINT CLASSIFICATION"
 app = FastAPI(title=APP_TITLE)
 
@@ -16,8 +18,14 @@ class Item(BaseModel):
 
 
 @app.post("/animal-footprint-classification")
-async def do_upload_file(image_path: Item):
-    return None
+async def do_upload_file(image_path: Item) -> dict:
+    logger.logger.info("Uploading image...{}".format(image_path.filePath))
+    try:
+        result = vistra_afp_classification(image_path.filePath)
+        return result
+    except Exception as ex:
+        logger.logger.error(str(ex))
+        raise ex
 
 
 if __name__ == "__main__":
